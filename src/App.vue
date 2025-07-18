4f98eb08eac3e1234780b31492e607b4d4108bec
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-07-18 12:44:51 +0600

    animation added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-bl from-indigo-200 to-indigo-50 p-4">
-    <h1 class="text-3xl font-bold text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="linear">
+    <h1 class="text-3xl font-bold text-center bg-gradient-to-r from-indigo-800 via-purple-600 to-pink-600 text-transparent bg-clip-text" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="linear">
       Welcome to Tailwind CSS with Vue.js!<br/>Rapidly build modern websites without ever leaving your HTML.
     </h1>
     <p class="mt-4 text-lg" data-aos="fade-down" 
