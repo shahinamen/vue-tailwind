150bc396aee5d07e7026de32195465f29ab14f4c
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-07-18 17:26:20 +0600

    animation started

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,6 @@
+<script setup>
+import { Camera, MoveRight } from 'lucide-vue-next';
+</script>
 <template>
   <section class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-bl from-indigo-200 to-indigo-50 p-4">
     <h1 class="text-3xl font-bold text-center bg-gradient-to-r from-indigo-800 via-purple-600 to-pink-600 text-transparent bg-clip-text" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="linear">
@@ -10,4 +13,105 @@
       Edit <code>src/App.vue</code> to see changes.
     </p>
   </section>
+  <section class="flex flex-col items-center justify-center bg-zinc-800 min-h-screen">
+    <h1 class="text-2xl md:text-4xl text-white mt-8" data-aos="slide-down" data-aos-duration="1000">Start animating</h1>
+    <p class="text-lg md:text-xl text-zinc-200" data-aos="slide-up" data-aos-duration="1000">Add <code>data-aos</code> attributes to your elements.</p>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6 mb-6">
+      <div class="bg-zinc-700 p-4 rounded-lg shadow-md cursor-pointer flex justify-between items-center w-sm border-1 border-zinc-700 hover:border-sky-500" data-aos="fade-up" data-aos-duration="1000">
+        <div class="flex items-center">
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+          <div class="pl-2 text-sky-500">Getting Started</div>
+        </div>
+        <MoveRight class="text-sky-500"/>
+      </div>
+      <div class="bg-zinc-700 p-4 rounded-lg shadow-md cursor-pointer flex justify-between items-center w-sm border-1 border-zinc-700 hover:border-sky-500" data-aos="fade-up" data-aos-duration="1000">
+        <div class="flex items-center">
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+          <div class="pl-2 text-sky-500">Getting Started</div>
+        </div>
+        <MoveRight class="text-sky-500"/>
+      </div>
+      <div class="bg-zinc-700 p-4 rounded-lg shadow-md cursor-pointer flex justify-between items-center w-sm border-1 border-zinc-700 hover:border-sky-500" data-aos="fade-up" data-aos-duration="1000">
+        <div class="flex items-center">
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+          <div class="pl-2 text-sky-500">Getting Started</div>
+        </div>
+        <MoveRight class="text-sky-500"/>
+      </div>
+      <div class="bg-zinc-700 p-4 rounded-lg shadow-md cursor-pointer flex justify-between items-center w-sm border-1 border-zinc-700 hover:border-sky-500" data-aos="fade-up" data-aos-duration="1000">
+        <div class="flex items-center">
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+          <div class="pl-2 text-sky-500">Getting Started</div>
+        </div>
+        <MoveRight class="text-sky-500"/>
+      </div>
+      <div class="bg-zinc-700 p-4 rounded-lg shadow-md cursor-pointer flex justify-between items-center w-sm border-1 border-zinc-700 hover:border-sky-500" data-aos="fade-up" data-aos-duration="1000">
+        <div class="flex items-center">
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+          <div class="pl-2 text-sky-500">Getting Started</div>
+        </div>
+        <MoveRight class="text-sky-500"/>
+      </div>
+      <div class="bg-zinc-700 p-4 rounded-lg shadow-md cursor-pointer flex justify-between items-center w-sm border-1 border-zinc-700 hover:border-sky-500" data-aos="fade-up" data-aos-duration="1000">
+        <div class="flex items-center">
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+          <div class="pl-2 text-sky-500">Getting Started</div>
+        </div>
+        <MoveRight class="text-sky-500"/>
+      </div>
+      <div class="bg-zinc-700 p-4 rounded-lg shadow-md cursor-pointer flex justify-between items-center w-sm border-1 border-zinc-700 hover:border-sky-500" data-aos="fade-up" data-aos-duration="1000">
+        <div class="flex items-center">
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+          <div class="pl-2 text-sky-500">Getting Started</div>
+        </div>
+        <MoveRight class="text-sky-500"/>
+      </div>
+      <div class="bg-zinc-700 p-4 rounded-lg shadow-md cursor-pointer flex justify-between items-center w-sm border-1 border-zinc-700 hover:border-sky-500" data-aos="fade-up" data-aos-duration="1000">
+        <div class="flex items-center">
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+          <div class="pl-2 text-sky-500">Getting Started</div>
+        </div>
+        <MoveRight class="text-sky-500"/>
+      </div>
+      <div class="bg-zinc-700 p-4 rounded-lg shadow-md cursor-pointer flex justify-between items-center w-sm border-1 border-zinc-700 hover:border-sky-500" data-aos="fade-up" data-aos-duration="1000">
+        <div class="flex items-center">
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-sky-500"></span>
+          </span>
+          <div class="pl-2 text-sky-500">Getting Started</div>
+        </div>
+        <MoveRight class="text-sky-500"/>
+      </div>
+    </div>
+  </section>
+  <section class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-bl from-indigo-200 to-indigo-50 p-4">
+    <h1 class="text-4xl text-center">Animation stareted</h1>
+  </section>
+
 </template>
