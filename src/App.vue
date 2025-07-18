41c054a0a24059c2babb9d6754d000e0e1a4351f
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-07-18 12:34:22 +0600

    animation added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
-<script setup>
-</script>
-
 <template>
-  <section class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-    <h1 class="text-3xl font-bold">
-    Rapidly build modern websites without ever leaving your HTML.
+  <section class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-bl from-indigo-200 to-indigo-50 p-4">
+    <h1 class="text-3xl font-bold text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text" data-aos="fade-up" data-aos-duration="1000" data-aos-easing="linear">
+      Welcome to Tailwind CSS with Vue.js!<br/>Rapidly build modern websites without ever leaving your HTML.
     </h1>
-    <p class="mt-4 text-lg">
-      This is a simple Vue.js application styled with Tailwind CSS.
-    </p>
-    <p class="mt-2 text-sm text-gray-600">
+    <p class="mt-4 text-lg" data-aos="fade-down" 
+    data-aos-duration="1000">This is a simple Vue.js application styled with Tailwind CSS.</p>
+
+    <p class="mt-2 text-sm text-gray-600" data-aos="zoom-in" data-aos-duration="1000">
       Edit <code>src/App.vue</code> to see changes.
     </p>
   </section>
 </template>
-
-<style scoped>
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'aos/dist/aos.css'; // Import AOS styles
+import AOS from 'aos';     // Import AOS script
 import App from './App.vue'
 
+AOS.init();
 createApp(App).mount('#app')
