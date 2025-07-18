50a1d1f82f7bf3d14fd745cc85367b67378fcade
Md. Shahinur Islam <shahin_amen@yahoo.com>
2025-07-18 10:50:39 +0600

    tailwind added

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,17 @@
 </script>
 
 <template>
-  <div>
-    <h1 class="text-center text-2xl md:text-3xl">This is Tailwind CSS only</h1>
-  </div>
+  <section class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <h1 class="text-3xl font-bold">
+    Rapidly build modern websites without ever leaving your HTML.
+    </h1>
+    <p class="mt-4 text-lg">
+      This is a simple Vue.js application styled with Tailwind CSS.
+    </p>
+    <p class="mt-2 text-sm text-gray-600">
+      Edit <code>src/App.vue</code> to see changes.
+    </p>
+  </section>
 </template>
 
 <style scoped>
